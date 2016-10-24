5b9ad8cbbf3649aa34639218be1ea1b447093116
109637 <109637@csbcnet.com.tw>
2016-10-24 16:54:41 +0800

    add Food filter

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -5,7 +5,11 @@ from restaurants.models import Restaurant, Food
 class RestaurantAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone_number', 'address')
 
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'restaurant', 'price')
+    list_filter = ('is_spicy',)
+
 admin.site.register(Restaurant, RestaurantAdmin)
-admin.site.register(Food)
+admin.site.register(Food, FoodAdmin)
 
 
