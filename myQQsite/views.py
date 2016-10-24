ee7c23830b0a5f944742b4462bcd615a226e4e64
109637 <109637@csbcnet.com.tw>
2016-10-24 13:38:48 +0800

    views and url practice

---
+++ myQQsite/views.py
@@ -0,0 +1,11 @@
+#encoding=utf-8
+from django.http import HttpResponse
+
+def here(request):
+    return HttpResponse('媽，我在這！')
+def math(request, a, b):
+    a = int(a)
+    b = int(b)
+    s, d, p, q = a+b, a-b, a*b, a/b
+    html = '<html>sum={s}<br>dif={d}<br>pro={p}<br>quo={q}</html>'.format(s=s, d=d, p=p, q=q)
+    return HttpResponse(html)
