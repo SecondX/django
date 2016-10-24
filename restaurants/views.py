45ddb19a9da8d4e718c223eee4c77d564ef85e83
109637 <109637@csbcnet.com.tw>
2016-10-24 15:34:19 +0800

    complete restaurant info in menu.html

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,15 +1,7 @@
 #encoding=utf-8
-from django.http import HttpResponse
-from django import template
-from django.template.loader import get_template
 from django.shortcuts import render_to_response
+from restaurants.models import Restaurant, Food
 
 def menu(request):
-    food1 = {
-            'name': '蕃茄炒蛋', 'price': 60, 'comment': '好吃', 'is_spicy': False
-            }
-    food2 = {
-            'name': '蒜泥白肉', 'price': 100, 'comment': '人氣推薦', 'is_spicy': True
-            }
-    foods = [food1, food2]
+    restaurants = Restaurant.objects.all()
     return render_to_response('menu.html', locals())
