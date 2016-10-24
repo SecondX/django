7dff92a31ff390c36d702775dfaf5c27293b872a
109637 <109637@csbcnet.com.tw>
2016-10-24 14:55:25 +0800

    after manage.py check and manage.py makemigrations restaurants

--- myQQsite/urls.py
+++ myQQsite/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from views import menu
-
+from restaurants.views import menu
 
 urlpatterns = [
     url(r'^menu/$', menu),
