e2d9c2d5dd6adaea605fca1d563a80e36e4f8a25
109637 <109637@csbcnet.com.tw>
2016-10-24 16:29:04 +0800

    create admin:root:123456789abc

--- myQQsite/urls.py
+++ myQQsite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from restaurants.views import menu
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^menu/$', menu),
+    url(r'^admin/', include(admin.site.urls)),
 ]
