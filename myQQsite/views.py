333ca1b576bf7ac59b47804c65c5eddbda8eb80c
109637 <109637@csbcnet.com.tw>
2016-10-24 14:07:17 +0800

    use autoload mechanism of template by setting.py

--- myQQsite/views.py
+++ myQQsite/views.py
@@ -1,6 +1,9 @@
 #encoding=utf-8
 from django.http import HttpResponse
 from django import template
+from django.template.loader import get_template
+
+
 
 def here(request):
     return HttpResponse('媽，我在這！')
@@ -9,7 +12,6 @@ def math(request, a, b):
     b = int(b)
     s, d, p, q = a+b, a-b, a*b, a/b
 
-    with open('templates/math.html', 'r') as reader:
-        t = template.Template(reader.read())
+    t = get_template('math.html')
     content = template.Context({'s':s, 'd':d, 'p':p, 'q':q})
     return HttpResponse(t.render(content))
