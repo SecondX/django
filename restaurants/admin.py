ce55276e8fbbd23e94feb8050862375503f435d1
109637 <109637@csbcnet.com.tw>
2016-10-24 16:57:54 +0800

    add search field

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -4,6 +4,7 @@ from restaurants.models import Restaurant, Food
 
 class RestaurantAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone_number', 'address')
+    search_fields = ('name', 'address')
 
 class FoodAdmin(admin.ModelAdmin):
     list_display = ('name', 'restaurant', 'price')
